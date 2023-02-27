36261cbd0b12c6473eb2adbce41d1b7c3e8e4834
fersie7e <fersie7e@gmail.com>
2023-02-27 17:04:34 +0100

    Creacion de los archivos pdf con la cabecera de la factura

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import glob
+import fpdf
+import pathlib
 
 
 filespath = glob.glob("invoices/*.xlsx")
 
 for filepath in filespath:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    pdf = fpdf.FPDF(orientation="P", unit="mm",format="A4")
+    pdf.add_page()
+
+    filename = pathlib.Path(filepath).stem
+    nro_factura = filename.split("-")[0]
+    fecha_factura = filename.split("-")[1]
+
+    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.cell(w=0, h=8, txt=f"Factura nro. {nro_factura}")
+    pdf.output(f"PDFs/{filename}.pdf")
     print(df)
